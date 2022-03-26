22e52b704a1a1932e978bf33c0b57717ff93b0ce
sersch1sersch <sersch11031984@gmail.com>
2022-03-27 00:07:40 +0300

    dz23

--- dz-13/index.js
+++ dz-13/index.js
@@ -9,41 +9,78 @@ function sbros(){
     info.innerHTML = ''
 }
 
-      function HashStorageFunk() {
-      let storage = {};
-        this.addValue = function(napitok,rezept,alcogol) {
-            var napitok=prompt('Ввeдите название напитка');
-        var rezept =prompt('Ввeдите рецепт напитка');
-        var alcogol =confirm('Напиток алкогольный или нет');
-        if(alcogol===true)
-        alcogol = 'да'
-        else 
-         alcogol = 'нет';
-        storage[napitok]={'рецепт':rezept,'алкогольный':alcogol}
-        }
-        this.getValue= function(napitok){
-          var napitok=prompt('Ввeдите название напитка');
-    
-          if(napitok in storage){
-            
-            info.textContent = 'напиток '+[napitok] + '\n' + JSON.stringify(storage[napitok])
-        }
-           else
-          return 'такого напитка нет в хранилище'
+     // function HashStorageFunk() {
+    //  let storage = {};
+    //    this.addValue = function(napitok,rezept,alcogol) {
+    //        var napitok=prompt('Ввeдите название напитка');
+    //    var rezept =prompt('Ввeдите рецепт напитка');
+    //    var alcogol =confirm('Напиток алкогольный или нет');
+    //    if(alcogol===true)
+    //    alcogol = 'да'
+    //    else 
+    //     alcogol = 'нет';
+    //    storage[napitok]={'рецепт':rezept,'алкогольный':alcogol}
+    //    }
+    //    this.getValue= function(napitok){
+     //     var napitok=prompt('Ввeдите название напитка');
+    //
+     //     if(napitok in storage){
+     //       
+     //       info.textContent = 'напиток '+[napitok] + '\n' + JSON.stringify(storage[napitok])
+      //  }
+      //     else
+     //     return 'такого напитка нет в хранилище'
            
-        }
-        this.deleteValue= function(napitok) {
-          var napitok=prompt('Ввeдите название напитка');
+      //  }
+      ///  this.deleteValue= function(napitok) {
+      //    var napitok=prompt('Ввeдите название напитка');
     
-          if(String(napitok) in storage)
-            return delete storage[napitok];
-            return false;
-        }
-        this.getKeys= function(){
-            info.innerHTML=(Object.keys(storage))
-        }
-      }
-      var drinkStorage=new HashStorageFunk();
+      //    if(String(napitok) in storage)
+      //      return delete storage[napitok];
+      //      return false;
+      //  }
+      //  this.getKeys= function(){
+      //      info.innerHTML=(Object.keys(storage))
+      //  }
+      //}
+      //var drinkStorage=new HashStorageFunk();
     
     
  
+      function HashStorageFunk() {
+        let storage = {};
+        var napitok=prompt('Ввeдите название напитка');
+        var rezept =prompt('Ввeдите рецепт напитка');
+        var alcogol =confirm('Напиток алкогольный или нет');
+        if(alcogol===true)
+          alcogol = 'да'
+          else 
+           alcogol = 'нет';
+          this.addValue = function(key,value) {
+              
+           storage[napitok][key]=value
+          }
+          this.getValue= function(key){
+            
+      
+            if(key in storage){
+              
+              info.textContent = storage[key]
+          }
+             else
+            return 'такого напитка нет в хранилище'
+             
+          }
+          this.deleteValue= function(key) {
+            var napitok=prompt('Ввeдите название напитка');
+      
+            if(String(key) in storage)
+              return delete storage[key];
+              return false;
+          }
+          this.getKeys= function(){
+              info.innerHTML=(Object.keys(storage))
+          
+        }
+      }
+        var drinkStorage=new HashStorageFunk();
--- dz-23/index.js
+++ dz-23/index.js
@@ -21,7 +21,134 @@ var formDef2=
   {label:'Возраст:',kind:'number',name:'age'},
   {caption:'Зарегистрироваться',kind:'submit'},
 ];
+function buildForm(fd){
+  
+for(let i=0;i<fd.length;i++){
+    let formItem=fd[i]
+switch(formItem.kind){
+    case'longtext':
+    let div = document.createElement('div')
+    let lab = document.createElement('label')
+lab.innerHTML=formItem.label
+let fe = document.createElement('input')
+fe.type=formItem.kind
+fe.name=formItem.name
+lab.appendChild(fe)
+div.appendChild(lab)
+form.appendChild(div)
+break;
 
-let forma = document.forms.form;
-forma.te
+case'number':
+let div2 = document.createElement('div')
+let lab2 = document.createElement('label')
+lab2.innerHTML=formItem.label
+let fe2 = document.createElement('input')
+fe2.type=formItem.kind
+fe2.name=formItem.name
+lab2.appendChild(fe2)
+div2.appendChild(lab2)
+form.appendChild(div2)
+break;
+
+case'shorttext':
+let div3 = document.createElement('div')
+let lab3 = document.createElement('label')
+lab3.innerHTML=formItem.label
+let fe3 = document.createElement('input')
+fe3.type=formItem.kind
+fe3.name=formItem.name
+lab3.appendChild(fe3)
+div3.appendChild(lab3)
+form.appendChild(div3)
+break;
+
+case'combo':
+let labs = document.createElement('label')
+let div1 = document.createElement('div')
+let sel=document.createElement('select')
+labs.innerHTML=formItem.label
+for(let o=0; o<formItem.variants.length;o++){
+let v=formItem.variants[o];
+let opt=document.createElement('option')
+opt.innerHTML=v.text;
+opt.value=v.value;
+sel.name=formItem.name
+sel.appendChild(opt);
+labs.appendChild(sel)
+div1.appendChild(labs)
+form.appendChild(div1)
+}
+break;
+
+case'radio':
+let div4 = document.createElement('div')
+let lab4 = document.createElement('label')
+lab4.innerHTML=formItem.label
+for(let r=0; r<formItem.variants.length;r++){
+let w=formItem.variants[r];
+let fe4=document.createElement('input')
+let lab5=document.createElement('label')
+fe4.type=formItem.kind
+fe4.name=formItem.name
+fe4.value=w.value
+fe4.style.marginRight='10px'
+fe4.checked = false;
+lab4.appendChild(fe4)
+lab4.appendChild(lab5)
+lab5.innerHTML=w.text
+div4.appendChild(lab4)
+form.appendChild(div4)
+}
+break;
+
+case'check':
+let div5 = document.createElement('div')
+let lab6 = document.createElement('label')
+lab6.innerHTML=formItem.label
+let fe5 = document.createElement('input')
+fe5.type='checkbox';
+fe5.name='votes';
+fe5.checked=true
+lab6.appendChild(fe5)
+div5.appendChild(lab6)
+form.appendChild(div5)
+break;
+
+case'memo':
+let div6 = document.createElement('div')
+let div7 = document.createElement('div')
+let lab7 = document.createElement('label')
+lab7.innerHTML=formItem.label
+let tex = document.createElement('textarea')
+tex.name='description';
+div6.appendChild(lab7)
+form.appendChild(div6)
+div7.appendChild(tex)
+form.appendChild(div7)
+break;
+
+case'submit':
+let div8 = document.createElement('div')
+let fe6 = document.createElement('input')
+fe6.value=formItem.caption
+fe6.type='submit'
+fe6.name='description';
+div8.appendChild(fe6)
+form.appendChild(div8)
+break;
+}
+}
+}
+buildForm(formDef1)
+buildForm(formDef2)
+
+
+
+
+
+
+
+
+
+
 
