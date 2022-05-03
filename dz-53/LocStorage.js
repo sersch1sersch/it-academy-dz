ef8819d8989b6929768b22e12739767ce6252137
Сергей Маргаев <96297248+sersch1sersch@users.noreply.github.com>
2022-05-03 20:12:56 +0300

    Add files via upload

---
+++ dz-53/LocStorage.js
@@ -0,0 +1,30 @@
+
+  
+  class LocStorageClass{
+    storage ={};
+     addValue (key,value) {
+       
+    this.storage[key]=value;
+       }
+     getValue(key){
+      if(key in this.storage)
+      return this.storage[key];
+     }
+     deleteValue(key) {
+           if(String(key) in this.storage)
+             return delete this.storage[key];
+             return false;
+         }
+         getKeys(){
+             console.log(Object.keys(this.storage))
+         }
+       }
+      
+       class LocStorageClass {
+         
+        setItem(key,value)
+         getItem(key)
+         removeItem(key)
+      }
+           let localStorage = new LocStorageClass();
+           let drinkStorage = new LocStorageClass();
